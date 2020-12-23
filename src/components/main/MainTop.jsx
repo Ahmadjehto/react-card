f15ee673173066a226967417d0bcd70e18651046
Ahmad <65040628+Ahmadjehto@users.noreply.github.com>
2020-12-23 21:42:44 +0300

    Add files via upload

---
+++ src/components/main/MainTop.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function MainTop() {
+  return (
+    <section className="py-5 text-center container">
+      <div className="row py-lg-5">
+        <div className="col-lg-6 col-md-8 mx-auto">
+          <h1 className="fw-light">AHMAD</h1>
+          <p className="lead text-muted">lorem</p>
+          <p>
+            <a href="#" className="btn btn-primary my-2">
+              1.
+            </a>
+            <a href="#" className="btn ms-4 btn-secondary my-2">
+              2.
+            </a>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default MainTop;
---
+++ src/data/entry-list.js
@@ -0,0 +1,46 @@
+const ENTRY_LIST = [
+  {
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2019/12/08/21/21/snow-4682266_960_720.jpg",
+    body:
+      "Snow comprises individual ice crystals that grow while suspended in the atmosphere—usually within clouds—and then fall, accumulating on the ground where they undergo further changes.",
+    like: 0
+  },
+  {
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2020/12/15/07/35/geese-5832933_960_720.jpg",
+    body:
+      "A goose is a bird of any of several waterfowl species in the family Anatidae. This group comprises the genera Anser (the grey geese and white geese) and Branta (the black geese). Some other birds, mostly related to the shelducks, have as part of their names.",
+    like: 0
+  },
+  {
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2012/06/19/10/32/owl-50267_960_720.jpg",
+    body:
+      "Owls are birds from the order Strigiformes /ˈstrɪdʒɪfɔːrmiːz/, which includes over 200 species of mostly solitary and nocturnal birds of prey typified by an upright stance, a large, broad head, binocular vision, binaural hearing, sharp talons, and feathers adapted for silent flight.",
+    like: 0
+  },
+  {
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2013/09/18/18/24/chocolate-183543_960_720.jpg",
+    body:
+      "Chocolate is a preparation of roasted and ground cacao seeds that is made in the form of a liquid, paste, or in a block, which may also be used as a flavoring ingredient in other foods.",
+    like: 0
+  },
+  {
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2020/11/16/18/20/fallow-deer-5749914_960_720.jpg",
+    body:
+      "A gazelle is any of many antelope species in the genus Gazella. This article also deals with the six species included in two further genera, Eudorcas and Nanger, which were formerly considered subgenera of Gazella.",
+    like: 0
+  },
+  {
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2020/12/09/13/59/gifts-5817555_960_720.jpg",
+    body:
+      "A gift or a present is an item given to someone without the expectation of payment or anything in return. An item is not a gift if that item is already owned by the one to whom it is given.",
+    like: 0
+  }
+];
+
+export default ENTRY_LIST;
